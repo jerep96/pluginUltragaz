c666c2d8fdbf2c03a9fef9c0987b5329fc490fbd
jerep96 <jeremiasp96@gmail.com>
2022-01-19 15:05:59 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'forms', component: FormularioComponent },
+  { path: '**', redirectTo: '' },
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    FormularioComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/activity.interface.ts
@@ -0,0 +1,5 @@
+export  interface activityList{
+    activityType : string
+    ul_cant_os : string
+    ul_os_json : string
+}
